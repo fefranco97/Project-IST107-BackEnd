0f79854feffb68496ed15a4fe0191b2af0f2d242
Felipe Franco <fe.franco97@gmail.com>
2024-11-11 16:35:20 -0800

    [Feat]: Created all the base functions

--- functions/src/api/createRecipe.js
+++ functions/src/api/createRecipe.js
@@ -1,7 +1,9 @@
 const { onRequest } = require('firebase-functions/v2/https')
 const logger = require('firebase-functions/logger')
 
-exports.createRecipe = onRequest((request, response) => {
+const CreateRecipe = onRequest((request, response) => {
   logger.info('Hello logs!', { structuredData: true })
   response.send('Create Recipe')
 })
+
+module.exports = { CreateRecipe }
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,19 +1,15 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { CreateRecipe } = require('./api/createRecipe')
+const { DeleteRecipe } = require('./api/deleteRecipe')
+const { EditRecipe } = require('./api/editRecipe')
+const { GetAllRecipes } = require('./api/getAllRecipes')
+const { GetRecipeDetails } = require('./api/getRecipeDetails')
+const { UpdateRecipe } = require('./api/updateRecipe')
+const { GetUserRecipes } = require('./api/getUserRecipes')
 
-const { onRequest } = require('firebase-functions/v2/https')
-const logger = require('firebase-functions/logger')
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-exports.helloWorld = onRequest((request, response) => {
-  logger.info('Hello logs!', { structuredData: true })
-  response.send('Hello from Firebase!')
-})
+exports.createRecipe = CreateRecipe
+exports.deleteRecipe = DeleteRecipe
+exports.editRecipe = EditRecipe
+exports.getAllRecipes = GetAllRecipes
+exports.getRecipeDetails = GetRecipeDetails
+exports.updateRecipe = UpdateRecipe
+exports.getUserRecipes = GetUserRecipes
