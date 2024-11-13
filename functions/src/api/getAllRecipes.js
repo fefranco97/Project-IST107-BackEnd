fa90013eb65c45dc9e72154fb961bb452d6f8ec5
Felipe Franco <fe.franco97@gmail.com>
2024-11-12 19:17:08 -0800

    Adding DB

--- functions/src/api/getAllRecipes.js
+++ functions/src/api/getAllRecipes.js
@@ -1,8 +1,18 @@
 const { onRequest } = require('firebase-functions/v2/https')
 const logger = require('firebase-functions/logger')
+const { database } = require('../config/firebase')
 
-const GetAllRecipes = onRequest((request, response) => {
+const GetAllRecipes = onRequest(async (request, response) => {
   logger.info('Hello logs!', { structuredData: true })
+
+  const { name, description, ingredients, steps, image } = request.body
+
+  const userCollection = collection(database, 'User')
+  const userSnapshot = await getDocs(userCollection)
+  const userList = userSnapshot.docs.map((doc) => doc.data())
+
+  console.log(userList)
+
   response.send('Create Recipe')
 })
 
--- functions/src/config/firebase.js
+++ functions/src/config/firebase.js
@@ -1,11 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 import { getAnalytics } from 'firebase/analytics'
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: 'AIzaSyCfVDyHw9-eiA-wP2aNToYBZRPFyWNQSfw',
   authDomain: 'projectrecipesist107.firebaseapp.com',
@@ -19,3 +16,9 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
+
+// Database setup
+
+const database = getFirestore(app)
+
+module.exports = { database }
