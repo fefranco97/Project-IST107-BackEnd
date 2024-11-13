9b34ec47193e7cd2a323e55e73380d223cdf8681
Felipe Franco <fe.franco97@gmail.com>
2024-11-12 20:35:41 -0800

    fix

--- functions/src/api/getAllRecipes.js
+++ functions/src/api/getAllRecipes.js
@@ -1,17 +1,14 @@
 const { onRequest } = require('firebase-functions/v2/https')
 const logger = require('firebase-functions/logger')
-const { database } = require('../config/firebase')
+const { firestore } = require('firebase-admin')
 
 const GetAllRecipes = onRequest(async (request, response) => {
   logger.info('Hello logs!', { structuredData: true })
 
-  const { name, description, ingredients, steps, image } = request.body
-
-  const userCollection = collection(database, 'User')
-  const userSnapshot = await getDocs(userCollection)
-  const userList = userSnapshot.docs.map((doc) => doc.data())
-
-  console.log(userList)
+  // const userCollection = collection(database, 'User')
+  // const userSnapshot = await getDocs(userCollection)
+  // const userList = userSnapshot.docs.map((doc) => doc.data())
+  // console.log(userList)
 
   response.send('Create Recipe')
 })
