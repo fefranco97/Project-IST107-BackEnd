ae95d3eb23a68367419b58d8a429b83ac2e13321
Edison <aleksander.nakata@gmail.com>
2024-11-27 03:03:01 +0000

    Solved response

--- functions/src/api/recipes/getRecipeDetails.js
+++ functions/src/api/recipes/getRecipeDetails.js
@@ -11,9 +11,9 @@ const GetRecipeDetails = onRequest((req, res) => {
         res.status(404).send("Recipe not found");
         return;
       }
-      res.status(200).json({ id: recipeDoc.id, ...recipeDoc.data() });
+      res.status(200).json({ status: "success", data: recipeDoc.data() });
     } catch (error) {
-      res.status(500).send(error.message);
+      res.status(500).send({ status: "error", message: error.message });
     }
   });
 });
